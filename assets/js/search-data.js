ca5ff5f7a985cb972e57ecc4e10086ff8aa0438d
chrystali2002 <51093651+chrystali2002@users.noreply.github.com>
2025-11-05 23:49:54 +0000

    Deploying to gh-pages from @ chrystali2002/olusegun.christiana.github.io@26dae00020fba19f0dca973f4ba7a5b55441f671 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-dr-christiana-olusegun",
-    title: "Dr. Christiana OLUSEGUN",
+    id: "nav-",
+    title: "",
     section: "Navigation",
     handler: () => {
       window.location.href = "/olusegun.christiana.github.io/";
